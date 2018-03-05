9ee71cd51e412fcb58b5a413455eee095fbba95b
root <root@DESKTOP-3OID0FF.localdomain>
2018-03-04 22:20:09 -0500

    Updated all files

---
+++ script.js
@@ -0,0 +1,42 @@
+//Code to generate quote from an array
+
+quote();
+
+function quote() {
+  $.ajax({
+    url: "http://api.forismatic.com/api/1.0/",
+    jsonp: "jsonp",
+    dataType: "jsonp",
+    data: {
+      method: "getQuote",
+      lang: "en",
+      format: "jsonp"
+    },
+    success: function(response) {
+      $('#quote').html(response.quoteText)
+      $('#author').html(response.quoteAuthor)
+    }
+  });
+}
+
+$("#quoteButton").on("click", function() {
+  quote();
+});
+
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id; 
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));
